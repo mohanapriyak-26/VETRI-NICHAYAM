46c20ac0a9850f5af9b670a75b9baca4d434f02e
mohanapriyak-26 <kavinpriya26@gmail.com>
2025-12-06 17:42:41 +0530

    file updated

--- Project_2_FLIGHT BOOKING/server/routes/customerRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const Router = express.Router();
-import { bookTicket, cancelTicket } from "../controllers/customerController.js";
-import { verifyToken } from "../middleware/authMiddleware.js";
-
-Router.post('/book-ticket', verifyToken, bookTicket);
-Router.put('/cancel-ticket/:id', verifyToken, cancelTicket);
-
-export default Router;
