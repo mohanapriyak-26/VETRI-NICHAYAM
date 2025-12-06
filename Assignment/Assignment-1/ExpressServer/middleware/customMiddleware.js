561f7bf635d4608c77c9ae2d79b2fea7303f170c
mohanapriyak-26 <kavinpriya26@gmail.com>
2025-12-06 17:57:35 +0530

    file changed

--- Assignment/Assignment-1/ExpressServer/middleware/customMiddleware.js
+++
@@ -1,9 +0,0 @@
-const customMiddleware =(req,res,next)=>{
-    let verify =false;
-    if(!verify){
-        res.send("user not verified");
-        return ;
-    }
-    next();
-};
-module.exports=customMiddleware;
